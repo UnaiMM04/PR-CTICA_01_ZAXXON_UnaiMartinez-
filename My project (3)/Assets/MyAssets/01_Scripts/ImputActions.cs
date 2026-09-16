1de1cd6da25504496f06b63ee8d9c512477f6410
Noaccloud <ainhoaccoronel1@gmail.com>
2026-09-16 12:08:35 +0200

    Funcina Desplazamiento
    Vamoooooosss, funciona el mando

---
+++ My project (3)/Assets/MyAssets/01_Scripts/ImputActions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+internal class imputActions
+{
+    internal static void Disable()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void Enable()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ My project (3)/Assets/MyAssets/01_Scripts/Script_01.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    bool isAlive;
+    public float speed;
+    Vector2 moveX;
+    
+
+    [SerializeField] float desplSpeedx;
+    [SerializeField] float desplSpeedy;
+
+    ImputActions imputActions;
+    
+
+    private void Awake()
+    {
+        imputActions = new ImputActions();
+        imputActions.Player.Move.performed += ctx => desplSpeedx = speed * ctx.ReadValue<Vector2>().x;
+        imputActions.Player.Move.performed += ctx => desplSpeedy = speed * ctx.ReadValue<Vector2>().y;
+        imputActions.Player.Move.canceled += _ => desplSpeedx = speed * 0;
+        imputActions.Player.Move.canceled += _ => desplSpeedy = speed * 0;
+
+
+    }
+    private void Update()
+    {
+        {
+            transform.Translate(Vector2.right * desplSpeedx * Time.deltaTime);
+            transform.Translate(Vector2.up * desplSpeedy * Time.deltaTime);
+        }
+    }
+
+    private void OnEnable()
+    {
+        imputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        imputActions.Disable();
+    }
+}

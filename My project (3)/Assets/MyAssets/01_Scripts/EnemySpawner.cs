1d571dbf855c367982abcab937963f7727246304
Noaccloud <ainhoaccoronel1@gmail.com>
2026-09-24 12:27:25 +0200

    aparecen varios enemigos en cada olead

--- My project (3)/Assets/MyAssets/01_Scripts/EnemySpawner.cs
+++ My project (3)/Assets/MyAssets/01_Scripts/EnemySpawner.cs
@@ -9,6 +9,12 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] float randomX;
     [SerializeField] float randomY;
 
+
+    [SerializeField] int minEnemiesPerWave = 1;  // Cantidad mínima de enemigos por oleada
+    [SerializeField] int maxEnemiesPerWave = 5;  // Cantidad máxima de enemigos por oleada
+    [SerializeField] float minScale = 0.5f;       // Tamaño mínimo (ej. 50%)
+    [SerializeField] float maxScale = 2.0f;       // Tamaño máximo (ej. 200%)
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -26,12 +32,29 @@ public class EnemySpawner : MonoBehaviour
         while (true)
         {
 
-            randomX = Random.Range(-20f, 20f);
-            randomY = Random.Range(-20f, 20f);
-            Vector3 despl = new Vector3(randomX, randomY, 0);
-            Vector3 instPost = transform.position + despl;
-            Instantiate( Enemigoo, instPost, Quaternion.identity);
+            int enemyCount = Random.Range(minEnemiesPerWave, maxEnemiesPerWave + 1);
+
+            for (int i = 0; i < enemyCount; i++)
+            {
+
+                randomX = Random.Range(-20f, 20f);
+                randomY = Random.Range(-20f, 20f);
+                Vector3 despl = new Vector3(randomX, randomY, 0);
+                Vector3 instPost = transform.position + despl;
+              //Instantiate(Enemigoo, instPost, Quaternion.identity);
+
+                // Instanciar el enemigo
+                GameObject newEnemy = Instantiate(Enemigoo, instPost, Quaternion.identity);
+
+                // 3. Aplicar tamaño (escala) aleatorio uniforme
+                float randomScale = Random.Range(minScale, maxScale);
+                newEnemy.transform.localScale = new Vector3(randomScale, randomScale, randomScale);
+            }
+
             yield return new WaitForSeconds(interval);
+
+           
+            //yield return new WaitForSeconds(interval);
         }
     }
 
